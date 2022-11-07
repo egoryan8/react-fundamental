d07b29180a9501761be2159c96e5f46da854f0c0
Fogel Egor <fogel_92@bk.ru>
2022-11-07 14:57:04 +0700

    feat: fetch posts, preloader

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import PostService from './API/PostService';
 import './App.css';
 import PostFilter from './components/PostFilter';
 import PostForm from './components/PostForm/PostForm';
 import PostsList from './components/PostsList/PostsList';
 import MyButton from './components/UI/button/MyButton';
 import MyModal from './components/UI/modal/MyModal';
+import Preloader from './components/UI/preloader/Preloader';
 import { usePosts } from './hooks/usePosts';
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'ааааа', body: 'qfqwfqwqw' },
-    { id: 2, title: 'ббб', body: 'цпцп' },
-    { id: 3, title: 'вв', body: 'ячыфмы' },
-    { id: 4, title: 'гг', body: 'р44прук' },
-  ]);
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ searchValue: '', sort: '' });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.searchValue);
+  const [isPostsLoading, setIsPostsLoading] = useState(null);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -25,6 +27,13 @@ function App() {
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
+
+  async function fetchPosts() {
+    setIsPostsLoading(true);
+    const posts = await PostService.getAll();
+    setPosts(posts);
+    setIsPostsLoading(false);
+  }
 
   return (
     <div className="App">
@@ -35,11 +44,11 @@ function App() {
         <PostForm create={createPost} setModal={setModal} />
       </MyModal>
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostsList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title={'Список постов про JavaScript'}
-      />
+      {isPostsLoading ? (
+        <Preloader />
+      ) : (
+        <PostsList remove={removePost} posts={sortedAndSearchedPosts} title={'Список постов'} />
+      )}
     </div>
   );
 }
